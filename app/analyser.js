16ec576636814f1767c02038c6cf8634b44ce806
Alistair Kung <alistaircckung@gmail.com>
2017-10-03 16:04:41 +0800

    created wordCounter module

--- app/analyser.js
+++ app/analyser.js
@@ -4,7 +4,11 @@ function Analyser(){
 
 Analyser.prototype.load = function (bookPath) {
   var fs = require("fs");
-  this.book = fs.readFileSync(bookPath).toString('utf-8').trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/[\n]/g, ' ').toLowerCase()
+  this.book = fs.readFileSync(bookPath)
+    .toString('utf-8').trim()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    .replace(/[\n]/g, ' ')
+    .toLowerCase()
 };
 
 module.exports = Analyser;

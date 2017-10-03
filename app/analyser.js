7880721f1842be0196e0f1859c28d49c061ca9c6
Alistair Kung <alistaircckung@gmail.com>
2017-10-03 16:51:03 +0800

    linked wordcounter module to book analyser

--- app/analyser.js
+++ app/analyser.js
@@ -1,14 +1,23 @@
+var wordCounter = require('../app/wordCounter')
+
 function Analyser(){
   this.book = null
+  this.wordCounter = new wordCounter
 }
 
 Analyser.prototype.load = function (bookPath) {
   var fs = require("fs");
   this.book = fs.readFileSync(bookPath)
-    .toString('utf-8').trim()
+    .toString('utf-8')
+    .trim()
     .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
     .replace(/[\n]/g, ' ')
     .toLowerCase()
 };
+
+Analyser.prototype.displayWordFrequencies = function() {
+  this.wordCounter.count(this.book)
+  return this.wordCounter.wordcount
+}
 
 module.exports = Analyser;
--- spec/analyserSpec.js
+++ spec/analyserSpec.js
@@ -4,12 +4,18 @@ describe("analyser", function () {
 
   beforeEach(function () {
     analyser = new Analyser()
+    analyser.load('spec/testbook.txt')
   })
 
   describe("#load", function () {
     it("should return a prepared string from the text file", function () {
-      analyser.load('spec/testbook.txt')
       expect(analyser.book).toBe('one one two three four four five')
+    })
+  })
+
+  describe("#displayWordFrequencies", function() {
+    it("should output each individual word appearing in the book and the frequency of occurences", function () {
+      expect(analyser.displayWordFrequencies()).toEqual({ "one": 2, "two": 1, "three": 1, "four": 2, "five": 1})
     })
   })
 
